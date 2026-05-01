c5337d2533cd79e48d23ad51295aa83f8ce8cae7
Raeh Manart H. Espino <raehmhespino27@gmail.com>
2026-05-02 00:06:03 +0800

    Upddated vite.config.js to include other html files in the build process.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      }
+    }
+  },
+  
+  // Tells Vite to build ALL your HTML files for Vercel
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        wish: resolve(__dirname, 'wish.html'),
         details: resolve(__dirname, 'details.html'),
-      },
-    },
-  },
-})
+        login: resolve(__dirname, 'login.html') // Add your new login page here too!
+      }
+    }
+  }
+});
